b55fa281bf28f3173e1a8aa59fe8fe642f675d45
David-Lisboa <lisboa.prog@gmail.com>
2021-03-18 14:34:50 -0400

    executando atraves do flask run

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,4 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello word"
+    return "Ola mundo"
--- microblog.py
+++ microblog.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-# app = Flask("microblog")
-
-# #aqui vai comentário
-# @app.route('/')
-# def index():
-#     return "Olá mundo"
-
-# app.run()
+from app import app
